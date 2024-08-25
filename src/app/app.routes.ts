53dd8562285569e4fa3295873e0c1357028877ec
joaotuneli56 <joaopedromouratuneli@gmail.com>
2024-08-25 19:25:49 -0300

    Pagina de HOmePage finalizada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,15 @@ import { Routes } from '@angular/router';
 import { ListagemComercialComponent } from './components/listagem-comercial/listagem-comercial.component';
 import { ListagemRHComponent } from './components/listagem-rh/listagem-rh.component';
 import { ListagemTIComponent } from './components/listagem-ti/listagem-ti.component';
-import { HomepageComponent } from './components/homepage/homepage.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/homepage/homepage.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
-  { path: 'home', component: HomepageComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'listagem-ti', component: ListagemTIComponent },
   { path: 'listagem-rh', component: ListagemRHComponent },
   { path: 'listagem-comercial', component: ListagemComercialComponent },
--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+import { ListagemComercialComponent } from '../listagem-comercial/listagem-comercial.component';
+import { ListagemRHComponent } from '../listagem-rh/listagem-rh.component';
+import { ListagemTIComponent } from '../listagem-ti/listagem-ti.component';
+import { Colaborador } from '../../Models/colaborador';
+import { DbServiceService } from './../../services/db-service.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ListagemComercialComponent,
+    ListagemRHComponent,
+    ListagemTIComponent,
+    CommonModule
+  ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
+export class HomeComponent implements OnInit {
+  colaboradores: any[] = [];
 
+  constructor(private dbService: DbServiceService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.dbService.getColaboradores().subscribe(data => {
+      this.colaboradores = data;
+    });
+  }
+
+  logout(): void {
+    // Implementar a lógica de logout
+    this.router.navigate(['/login']);
+  }
 }
