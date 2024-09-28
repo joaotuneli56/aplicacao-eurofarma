51ca8f28edfd048b4cae5bc8f85d4a6cb990779b
joaotuneli56 <joaopedromouratuneli@gmail.com>
2024-09-28 17:55:02 -0300

    Estilizando graficos do home gestor

--- src/app/components/homegestor/homegestor.component.ts
+++ src/app/components/homegestor/homegestor.component.ts
@@ -4,6 +4,7 @@ import { Router, RouterOutlet } from '@angular/router';
 import { Colaborador } from '../../Models/colaborador';
 import { DbServiceService } from './../../services/db-service.service';
 import { CommonModule } from '@angular/common';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-homegestor',
@@ -19,7 +20,9 @@ export class HomeGestorComponent implements OnInit {
   colaboradores: Colaborador[] = [];
   totalColaboradores: number = 0;
   colaboradoresDoDepartamento: Colaborador[] = [];
-  departamentoGestor: string = '';  // Será definido dinamicamente
+  departamentoGestor: string = '';
+  totalColaboradoresbarra = 50;
+  colaboradoresAtivos = 30;
 
   constructor(
     private dbService: DbServiceService,
@@ -51,4 +54,28 @@ export class HomeGestorComponent implements OnInit {
     this.AuthService.logout(); // Chama o método de logout do AuthService
     this.router.navigate(['/login']);
   }
+
+  createChart() {
+    const ctx = document.getElementById('colaboradoresChart') as HTMLCanvasElement;
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Dept A', 'Dept B', 'Dept C'],
+        datasets: [{
+          label: 'Número de Colaboradores',
+          data: [10, 20, 30],
+          backgroundColor: ['#0D4B8A', '#007bff', '#28a745'],
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
 }
